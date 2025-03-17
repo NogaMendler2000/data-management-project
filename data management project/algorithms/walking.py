f19f3a89aaaa7d37635fdd7ceeea33e64e94cb18
Noga Mendler <78665396+NogaMendler2000@users.noreply.github.com>
2025-03-18 01:21:32 +0200

    Update walking.py

--- data management project/algorithms/walking.py
+++ data management project/algorithms/walking.py
@@ -55,10 +55,6 @@ def p_walk_algorithm(trip_df, threshold):
             if count < threshold:
                 # אם הצומת לא מכוסה, הוא MUP
                 mups.append(current_node)
-                # גיזום כל הילדים של הצומת (כי הם גם חייבים להיות לא מכוסים)
-                for duration_range in child_relations[current_node]:
-                    if duration_range not in visited:
-                        visited.add(duration_range)  # סימון הילדים כנגזמים
                 # הוספת ההורים של הצומת לתור העדיפויות
                 for duration_range in parent_relations[current_node]:
                     heapq.heappush(priority_queue, (-priority_score(duration_range), duration_range, "duration"))
@@ -67,18 +63,16 @@ def p_walk_algorithm(trip_df, threshold):
                 for duration_range in child_relations[current_node]:
                     if duration_range not in visited:
                         visited.add(duration_range)  # סימון ההורים כנגזמים
-                # הוספת הילדים של הצומת לתור העדיפויות
-                for duration_range in parent_relations[current_node]:
-                    heapq.heappush(priority_queue, (-priority_score(duration_range), duration_range, "duration"))
+               
         elif layer == "duration":
             count = duration_range_counts[current_node]
             if count < threshold:
                 # אם הצומת לא מכוסה, הוא MUP
                 mups.append(current_node)
                 # גיזום כל הילדים של הצומת (כי הם גם חייבים להיות לא מכוסים)
-                for hour_range in child_relations[current_node]:
-                    if hour_range not in visited:
-                        visited.add(hour_range)  # סימון הילדים כנגזמים
+                for start_station in child_relations[current_node]:
+                    if start_station not in visited:
+                        visited.add(start_station)  # סימון הילדים כנגזמים
                 # הוספת ההורים של הצומת לתור העדיפויות
                 for hour_range in parent_relations[current_node]:
                     heapq.heappush(priority_queue, (-priority_score(hour_range), hour_range, "hour"))
@@ -88,8 +82,8 @@ def p_walk_algorithm(trip_df, threshold):
                     if hour_range not in visited:
                         visited.add(hour_range)  # סימון ההורים כנגזמים
                 # הוספת הילדים של הצומת לתור העדיפויות
-                for hour_range in parent_relations[current_node]:
-                    heapq.heappush(priority_queue, (-priority_score(hour_range), hour_range, "hour"))
+                for duration_range in parent_relations[current_node]:
+                    heapq.heappush(priority_queue, (-priority_score(duration_range), duration_range, "duration"))
         elif layer == "hour":
             count = hour_range_counts[current_node]
             if count < threshold:
@@ -99,10 +93,11 @@ def p_walk_algorithm(trip_df, threshold):
                 for duration_range in child_relations[current_node]:
                     if duration_range not in visited:
                         visited.add(duration_range)  # סימון הילדים כנגזמים
+                
             else:
-                # אם הצומת מכוסה, גיזום כל ההורים שלו (כי הם גם חייבים להיות מכוסים)
+        
+                # הוספת הילדים של הצומת לתור העדיפויות אם הצומת מכוסה, 
                 for duration_range in child_relations[current_node]:
-                    if duration_range not in visited:
-                        visited.add(duration_range)  # סימון ההורים כנגזמים
+                    heapq.heappush(priority_queue, (-priority_score(duration_range), duration_range, "hour"))
     
     return mups  # החזרת רשימת MUPs
